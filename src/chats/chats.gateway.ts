453ef069d8400227e36c76f8a1a24e6cad7e6fd6
adx <nogo22@naver.com>
2022-01-22 16:16:06 +0900

    socket lifecycle

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -2,12 +2,34 @@ import { Socket } from 'socket.io';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: '/chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  //최초실행
+  afterInit() {
+    this.logger.log('init...');
+  }
+  //소켓 연결시
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    //nsp(namespace)
+    this.logger.log(`connected: ${socket.id}, ${socket.nsp.name}`);
+  }
+  //소켓 종료시
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconnected: ${socket.id}, ${socket.nsp.name}`);
+  }
+
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
